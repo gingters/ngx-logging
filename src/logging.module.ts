fcd76b6348e602cf0cb3f92942968747ad0a3f49
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2017-03-23 13:16:42 +0100

    Initial commit

---
+++ src/logging.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {LoggingService} from './logging.service';
+
+@NgModule({
+    declarations: [],
+    exports: [LoggingService],
+})
+export class LoggingModule {}
